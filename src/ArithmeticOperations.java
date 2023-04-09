98e83c75288afd73c2e1c90afee42e1a8614c059
Egor <egorgulaev2@gmail.com>
2023-04-09 16:04:29 +0300

    Поменял пол

--- src/ArithmeticOperations.java
+++ src/ArithmeticOperations.java
@@ -4,12 +4,18 @@ public class ArithmeticOperations {
     public static void main(String[] args) {
 
         System.out.println(drawisMonitorsCounter(12, 5));
+        System.out.println(Govno.egor(10, 10));
 
     }
+
     static int drawisMonitorsCounter(int monitors, int programmers) {
         return monitors % programmers;
     }
-
+    static class Govno {
+        static int egor(int s, int e) {
+            return s-e;
+        }
+    }
 }
 
 
