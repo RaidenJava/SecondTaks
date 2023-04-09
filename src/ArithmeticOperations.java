f57e695a39278647b57e1c4ea029909229fe310e
Egor <egorgulaev2@gmail.com>
2023-04-09 15:45:32 +0300

    2

---
+++ src/ArithmeticOperations.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+
+        System.out.println(drawisMonitorsCounter(12, 5));
+
+    }
+    static int drawisMonitorsCounter(int monitors, int programmers) {
+        return monitors % programmers;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/PrimitiveTypes.java
@@ -0,0 +1,13 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        byte varbyte = 120;
+        short varshort = 125;
+        int varint = 100000;
+        long varlong = 100000000l;
+        double vardouble = 1000.1;
+        float varfloat = 1000.7F;
+        boolean varboolean = true;
+        char varchar = 'a';
+
+    }
+}
